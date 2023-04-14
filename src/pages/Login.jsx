300ce08633eca0cfbf64a7eb02ab59e354c80d03
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-04-14 21:43:55 +0300

    Add React Hook Forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,34 @@
+import { useForm } from "react-hook-form";
+
 export const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    // console.log(`watch`, watch());
+    // console.log(`errors`, errors);
+
     return (
         <main>
             <h1>Login</h1>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-                laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-                voluptatum voluptate.
-            </p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    {...register("email", {
+                        required: "Email is required"
+                    })}
+                    label="Email"
+                    placeholder="E-mail"
+                />
+                <p>{errors.email?.message}</p>
+                <input
+                    {...register("password", {
+                        required: "Password is required"
+                    })}
+                    label="Password"
+                    placeholder="Password"
+                />
+                <p>{errors.password?.message}</p>
+                <input type="submit" />
+            </form>
         </main>
     );
 };
