57aacbf6b5f1ffff81ed88dcaf45bf1560a44953
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-04-17 23:39:35 +0300

    Add persistor

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Provider } from "react-redux";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
 
-import  store from "./redux/store";
+import  {store, persistor} from "./redux/store";
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./contacts/filtersSlice";
 import { authReducer } from './auth/slice';
 
-const store = configureStore({
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+];
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
+
+export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistReducer(authPersistConfig, authReducer),
         contacts: contactsReducer,
         filters: filterReducer,
     },
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
 });
 
-export default store;
+export const persistor = persistStore(store);
