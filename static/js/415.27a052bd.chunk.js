78f89dac9e13a7a22b8c3f3284a3787e30f1c483
Andrew-Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-04-19 15:59:26 +0000

    Deploying to gh-pages from @ Andrew-Samoilov/goit-react-hw-08-phonebook@a7ad971901463bbb7dac54bb875458bd28f65bc0 🚀

---
+++ static/js/415.27a052bd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,n,r){r.r(n),r.d(n,{default:function(){return t}});var a=r(3329);function t(){return(0,a.jsx)("main",{children:(0,a.jsxs)("h1",{children:["Contacts manager welcome page"," ",(0,a.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=415.27a052bd.chunk.js.map
